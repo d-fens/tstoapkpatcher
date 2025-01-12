1fa51c1a17bd043bf833b2ef8035934b87928869
d-fens <d-fens@users.noreply.github.com>
2025-01-12 21:59:18 +0000

    Force zipalign and generate protofiles

--- patch.sh
+++ patch.sh
@@ -16,9 +16,16 @@ grep -rl "https://syn-dir.sn.eamobile.com" "$SOURCE_OUTPUT" | xargs -r sed -i "s
 
 echo "Recompiling APK"
 java -jar /app/apktool.jar b /apk/decompiled/ -o /apk/patched.apk
-echo "Zipalign APK"
-/app/android-14/zipalign -v 4 /apk/patched.apk /apk/patched_zipalign.apk
+echo "Zipalign APK" # force the generation if file exists
+/app/android-14/zipalign -v 4 /apk/patched.apk /apk/patched_zipalign.apk -f
+# remove apk to avoid confusion
+rm -rf /apk/patched.apk
 
 echo "Signing APK"
 { echo "keystorepassword"; echo "keystorepassword"; echo ''; echo ''; echo ''; echo ''; echo ''; echo ''; echo 'yes'; } | keytool -genkeypair -alias key0 -keyalg RSA -keysize 2048 -validity 10000 -keystore /apk/mykeystore.jks
 { echo "keystorepassword"; } | /app/android-14/apksigner sign --ks-key-alias key0 --ks /apk/mykeystore.jks /apk/patched_zipalign.apk
+
+echo "Generating proto files (development)"
+mkdir -p /apk/proto
+/app/protodec/protodec --grab /apk/decompiled/lib/arm64-v8a/libscorpio.so
+mv *.proto /apk/proto
